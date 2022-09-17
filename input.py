2689e9952df285dd192d50824d12cce89db624b5
Clement BIEBER <clement.bieber@epita.fr>
2022-09-17 16:13:29 +0200

    handling of the entry with params and files

--- input.py
+++ input.py
@@ -11,8 +11,8 @@ class MainFrame(ttk.Frame):
     def __init__(self, container):
         super(MainFrame, self).__init__(container)
         self.listbox = None
+        self.list_items = tk.Variable(value=[])
         self.config_file = None
-        self.counter = 0
         self.main_window()
 
     def main_window(self):
@@ -25,8 +25,7 @@ class MainFrame(ttk.Frame):
         list_frame = ttk.Frame(self)
         list_frame['padding'] = (5, 10)
         list_frame.pack(expand=True, fill=tk.BOTH)
-        list_items = tk.Variable(value=[])
-        self.listbox = tk.Listbox(list_frame, listvariable=list_items, height=10)
+        self.listbox = utils.Listbox(list_frame, listvariable=self.list_items, height=10)
         self.listbox.pack(expand=True, side=tk.LEFT, fill=tk.BOTH)
 
         button_frame = tk.Frame(list_frame)
@@ -42,20 +41,29 @@ class MainFrame(ttk.Frame):
         loadconf_button.pack(side=tk.TOP)
 
     def edit_entry(self):
-        self.entry_window('entry')
+        idx = self.listbox.curselection()
+        print(idx)
+        if not idx:
+            mb.showerror('Error', 'You must select an entry to edit')
+        else:
+            idx = idx[0]
+            entry = self.listbox.get_value(idx)
+            self.entry_window(entry)
+            self.listbox.delete(idx)
 
     def entry_window(self, entry=None):
-        entryWin = EntryWindow(self, entry is None, entry)
+        entryWin = EntryWindow(self, entry)
         entry = entryWin.get_entry()
         if entry is not None:
-            self.listbox.insert(tk.END, f'{entry} {self.counter}')
-            self.counter += 1
+            self.listbox.insert(tk.END, entry)
 
     def remove_entry(self):
         idx = self.listbox.curselection()
+        print(idx)
         if not idx:
             mb.showerror('Error', 'You must select an entry to delete')
         elif mb.askyesno('Verify', 'Are you sure you want to delete the selected entry?'):
+            idx = idx[0]
             self.listbox.delete(idx)
 
     def load_config_file(self):
@@ -64,10 +72,10 @@ class MainFrame(ttk.Frame):
 
 class EntryWindow(tk.Toplevel):
 
-    def __init__(self, parent, new, entry):
+    def __init__(self, parent, entry):
         super(EntryWindow, self).__init__(parent)
-        self.new = new
-        self.entry = entry
+        self.new = entry is None
+        self.entry = utils.Entry() if self.new else entry
 
         self.txtInputs = []
         self.saveButton = tk.Button(self, text='SAVE', command=self.save_entry)
@@ -78,14 +86,41 @@ class EntryWindow(tk.Toplevel):
         self.title(f'{title_base} entry')
         self.attributes('-topmost', 1)
 
-        if not self.new:
-            self.load_entry()
-
         f = ttk.Frame(self)
         f['padding'] = (5, 10)
         f.pack(fill=tk.BOTH)
+        self.add_file_input(f)
         self.add_text_inputs(f)
         self.saveButton.pack(side=tk.BOTTOM)
+
+        if not self.new:
+            self.load_entry(self.entry)
+
+    def add_file_input(self, mainFrame):
+        measuredFrame = tk.Frame(mainFrame)
+        lbl = tk.Label(measuredFrame, text='Measured data')
+        lbl.pack(side=tk.LEFT)
+        lbl = tk.Label(measuredFrame, text=self.entry.measured, bg='white')
+        lbl.pack(side=tk.LEFT, fill=tk.X, expand=True)
+        load_button = tk.Button(measuredFrame, text='Browse', command=self.select_measured_file)
+        load_button.pack(side=tk.LEFT)
+        measuredFrame.pack(side=tk.TOP, fill=tk.Y, expand=True)
+        retinoFrame = tk.Frame(mainFrame)
+        lbl = tk.Label(retinoFrame, text='Retinotopic map MRI')
+        lbl.pack(side=tk.LEFT)
+        lbl = tk.Label(retinoFrame, text=self.entry.retino_map, bg='white')
+        lbl.pack(side=tk.LEFT, fill=tk.X, expand=True)
+        load_button = tk.Button(retinoFrame, text='Browse', command=self.select_retino_file)
+        load_button.pack(side=tk.LEFT)
+        retinoFrame.pack(side=tk.TOP, fill=tk.Y, expand=True)
+
+    def select_measured_file(self):
+        f = utils.select_file(self.entry.measured)
+        self.entry.measured = utils.Path(f)
+
+    def select_retino_file(self):
+        f = utils.Path(utils.select_file(self.entry.retino_map))
+        self.entry.retino_map = f
 
     def add_text_inputs(self, mainFrame):
         for field in utils.simulation_params._fields:
@@ -97,13 +132,13 @@ class EntryWindow(tk.Toplevel):
             txt.pack(side=tk.LEFT, fill=tk.BOTH)
             f.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-    def load_entry(self):
-        return  # TODO: function to load the entry if editing
+    def load_entry(self, entry):
+        params = entry.params
+        for i in range(len(params)):
+            self.txtInputs[i].insert(0, params[i])
 
     def save_entry(self):
-        self.entry = ''
-        for ipt in self.txtInputs:
-            self.entry += ipt.get()
+        self.entry.params = utils.simulation_params._make(list(map(lambda x: x.get(), self.txtInputs)))
         self.destroy()
 
     def get_entry(self):
--- utils.py
+++ utils.py
@@ -1,9 +1,22 @@
 from collections import namedtuple  # available in Python 2.6+
+import tkinter as tk
 from tkinter import filedialog as fd
-
+from dataclasses import dataclass
+from pathlib import Path
 
 simulation_params = namedtuple("simulation_params",
                                ["freq_temp", "freq_spacial", "amplitude", "phase_offset"])
+
+
+@dataclass
+class Entry:
+    """Data class corresponding to the entry structure"""
+    measured: Path = Path('/')
+    retino_map: Path = Path('/')
+    params: simulation_params = None
+
+    def set_params(self, param_list):
+        self.params = simulation_params(*param_list)
 
 
 def select_file(dir):
@@ -15,3 +28,29 @@ def select_file(dir):
         title='Open a config file',
         initialdir='/' if dir is None else dir,
         filetypes=filetypes)
+
+
+def etoile(*elements):
+    print(elements)
+
+
+class Listbox(tk.Listbox):
+    def __init__(self, master=None, cnf={}, **kw):
+        super(Listbox, self).__init__(master, cnf, **kw)
+        self.value = []
+
+    def insert(self, index, *elements):
+        super(Listbox, self).insert(index, elements)
+        self.value += list(elements)
+
+    def delete(self, first, last=None):
+        super(Listbox, self).delete(first, last)
+        if last:
+            while first < last:
+                self.value.pop(first)
+                first += 1
+        else:
+            self.value.pop(first)
+
+    def get_value(self, idx):
+        return self.value[idx]
