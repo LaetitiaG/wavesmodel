3a4ffa02c407f31cb46a69eec4b8b9fa400fdd63
Clement BIEBER <clement.bieber@epita.fr>
2023-01-04 15:34:44 +0100

    handling some logic, and arguments errors (wring paths of type errors)

--- toolbox/main.py
+++ toolbox/main.py
@@ -1,22 +1,48 @@
 import argparse
+from pathlib import Path
+
+
+def __check_stim(string):
+    if string not in ['TRAV_OUT', 'STANDING', 'TRAV_IN']:
+        raise argparse.ArgumentTypeError('Must be one of TRAV_OUT, STANDING, TRAV_IN')
+    return string
+
+
+def __path(string):
+    try:
+        return Path(string)
+    except Exception as e:
+        raise argparse.ArgumentTypeError(f'Error: {e}')
+
+
+def __path_or_list(string):
+    p = __path(string)
+    if p.exists():
+        return p
+    try:
+        return list(map(int, string.split(',')))
+    except ValueError:
+        # If the string cannot be parsed as a list of integers, raise an error
+        raise argparse.ArgumentTypeError('Must be a path or a list of integers separated by commas')
 
 
 def parse_cli():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--entry-config', required=True,
+    parser.add_argument('--entry-config', required=True, type=__path,
                         help='the path of an entry config file containing entries with all values')
-    parser.add_argument('--sensor-file', required=False, help='the path of the measured data file')
-    parser.add_argument('--mri-path', required=False,
+    parser.add_argument('--sensor-file', required=False, type=__path,
+                        help='the path of the measured data file')
+    parser.add_argument('--mri-path', required=False, type=__path,
                         help='the path where to find freesurfer output, and the forward model following the'
                              'architecture of the documentation')
-    parser.add_argument('--stim', required=False, default='TRAV_OUT',
+    parser.add_argument('--stim', required=False, default='TRAV_OUT', type=__check_stim,
                         help='a string corresponding to the type of simulation')
-    parser.add_argument('--sim-config', required=False,
+    parser.add_argument('--sim-config', required=False, type=__path_or_list,
                         help='the path of the simulation config file to be used to load simulation parameters, '
-                             'or a list of values corresponding to the simulation parameters')
-    parser.add_argument('--screen-config', required=False,
+                             'or a list of integers separated by commas')
+    parser.add_argument('--screen-config', required=False, type=__path_or_list,
                         help='the path of the screen config file to be used to load screen parameters, or a list of '
-                             'values corresponding to the screen parameters')
+                             'integers separated by commas')
     parser.add_argument('--gui', action='store_true', help='run the GUI mode of the toolbox')
 
     args = parser.parse_args()
