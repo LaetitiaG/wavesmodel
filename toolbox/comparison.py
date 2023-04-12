correct amplitude ratio calculation across time by taking the log

toolbox/comparison.py
@@ -149,9 +149,9 @@ def compare_meas_simu(entry, ev_proj):
                 # loop across rows
                 for r in range(len(inds)):
                     for c in range(len(inds)):  # loop across column
-                        cov_ch[i, r, c] = np.mean(ampls_ch[r] / ampls_ch[c])  # average across time
+                        cov_ch[i, r, c] = np.mean(np.log(ampls_ch[r] / ampls_ch[c]))  # average across time
 
-        cov_amp.append(np.log(cov_ch))
+        cov_amp.append(cov_ch)
 
         # Correlate measured vs predicted matrix
         msk_tri = np.triu(np.ones((len(inds), len(inds)), bool), k=1)  # zero the diagonal and all values below