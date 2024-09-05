bf57271e5d4f0de251cb3374a35fe5811b7965ea
LaetitaG <laetitia.grabot@gmail.com>
2024-09-05 17:34:55 +0200

    clean minimal_example

--- minimal_example.py
+++ minimal_example.py
@@ -5,36 +5,34 @@ Created on Tue Sep  5 15:54:11 2023
 @author: laeti
 """
 
-
 from toolbox.simulation import create_sim_from_entry
 from toolbox.projection import project_wave
 from toolbox.comparison import compare_meas_simu
-from toolbox.comparison import compare_meas_simu_V2
+from toolbox.comparison import compare_meas_simu_concat
 from toolbox.entry import Entry
 
-subject = 'QNP39U'
-session = 'session2'
 
-# my path
-wdir = "D:/Data/wave_model/" 
-sensorspath = wdir + 'data_MEEG/sensors/' 
+# My path TO CHANGE ACCORDINGLY
+wdir = "D:/Data/wave_model/scripts_python/example_dataset/" 
+subject = 'EWTO6I'
 
+## Specify the parameters of the model you want to test within an entry object (see help for Entry object to have the details of required parameters)
+entry = Entry()
+# Screen parameters
+width = 1920 # pixels
+height = 1080 # pixels
+distancefrom = 78 # cm
+heightcm = 44.2 # cm
+entry.screen_params = [width, height, distancefrom, heightcm]
 
-# Minimal example
+# Waves parameters
+freq_temp = 5 # Hz
+freq_spatial = 0.05 # cycles per mm of cortex
+amplitude = 1e-08 # A.m
+phase_offset = 1.5708 # rad
+decay = 0 # between 0 (no decay) to 1 (full decay)
+e0 = 5 # degree of visual angle (DVA)
 
-entry = Entry(c_space='full')
-width = 1920
-height = 1080
-distancefrom = 78
-heightcm = 44.2
-entry.screen_params = [width,height,distancefrom,heightcm]
-freq_temp = 5
-freq_spatial = 0.05
-amplitude = 1e-08
-phase_offset = 1.5707963267948966
-decay = 0
-e0 = 5
-verbose = True 
 condition_meas = 'trav_out' 
 condition_simu = 'trav_out' # 'trav_out' 'trav_in' 'stand' 'fov_out'
 if condition_simu == 'trav_out' :
@@ -50,14 +48,19 @@ elif condition_simu == 'fov_out':
     entry.stim = 'TRAV_OUT'
     entry.c_space = 'fov'  
 entry.simulation_params = [freq_temp, freq_spatial, amplitude, phase_offset, decay, e0]
-entry.freesurfer = wdir + 'data_MRI/preproc/freesurfer/' + subject
-entry.forward_model = wdir + 'data_MEEG/preproc/'+ subject+'/forwardmodel/' + subject + '_session2_ico5-fwd.fif'
-entry.measured = wdir + 'data_MEEG/sensors/' + subject + '_' + condition_meas +'-ave.fif'
-sim = create_sim_from_entry(entry)
-stc = sim.generate_simulation()
-ev_proj = project_wave(entry, stc, verbose)
 
-compare = compare_meas_simu(entry, ev_proj, SNR_threshold = 1)
-compare = compare_meas_simu_V2(entry, ev_proj)
-compare[7]
+# Paths parameters
+entry.freesurfer = wdir + 'data_MRI/preproc/freesurfer/' + subject
+entry.forward_model = wdir + 'data_MEEG/preproc/'+ subject+'/forwardmodel/' + subject + '_ico5-fwd.fif'
+entry.measured = wdir + 'data_MEEG/sensors/' + subject + '_' + condition_meas +'-ave.fif'
+
+## Launch modeling
+sim = create_sim_from_entry(entry)
+stc = sim.generate_simulation() # simulate sources
+ev_proj = project_wave(entry, stc) # predicted sensors activity
+
+## Compare predicted and measured data
+compare = compare_meas_simu(entry, ev_proj)
+R = compare[7] # correlation coefficient between predicted and measured data
+
         
